2abbd2b5327772c4ebfaf5b9a67d17a3984ed713
whuck <70232597+whuck@users.noreply.github.com>
2021-03-08 22:21:00 -0600

    assign05 complete

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -13,5 +13,7 @@ namespace Module5.Controllers
 
         //public IActionResult Index() => View(_bloggingContext.Blogs);
         public IActionResult Category() => View(_northwindContext.Categories.OrderBy(b=>b.CategoryName));
+
+        public IActionResult Index(int id) => View(_northwindContext.Products.Where(p=>p.CategoryId==id && !p.Discontinued));
     }
 }
